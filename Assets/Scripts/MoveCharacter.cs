8852b4a189e7dd3c385eb73b5078a59d738495f8
İzzet Can Suzen <izzetcansuzen@gmail.com>
2024-09-18 22:04:03 +0300

    alpha-v.1.0.2 feat: Add Character Jump Movement

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MoveCharacter : MonoBehaviour
 {
     private float characterSpeed = 5f;
+    private float characterJumpSpeed = 10f;
 
     void Start()
     {
@@ -17,9 +18,11 @@ public class MoveCharacter : MonoBehaviour
         //Oyuncu yürüme tuşlarına basınca girilen değeri al
         float verticalSpeed = Input.GetAxis("Vertical");
         float horizontalSpeed = Input.GetAxis("Horizontal");
+        float jumpSpeed = Input.GetAxis("Jump");
 
         //Oyuncu tıklamasına göre karakteri yönlendir
         transform.Translate(Vector3.forward * verticalSpeed * characterSpeed * Time.deltaTime);
         transform.Translate(Vector3.right * horizontalSpeed * characterSpeed * Time.deltaTime);
+        transform.Translate(Vector3.up * jumpSpeed * characterJumpSpeed * Time.deltaTime);
     }
 }
