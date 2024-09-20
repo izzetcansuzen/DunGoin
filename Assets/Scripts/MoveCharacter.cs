185a832ef8478c53ba03365e285456c1bd9711d8
İzzet Can Suzen <izzetcansuzen@gmail.com>
2024-09-20 08:15:25 +0300

    alpha-v-1.0.1 feat: Add character move animation

--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class MoveCharacter : MonoBehaviour
 {
+    Animator animator;
     private float characterSpeed = 5f;
     private float characterJumpSpeed = 10f;
 
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -24,5 +25,22 @@ public class MoveCharacter : MonoBehaviour
         transform.Translate(Vector3.forward * verticalSpeed * characterSpeed * Time.deltaTime);
         transform.Translate(Vector3.right * horizontalSpeed * characterSpeed * Time.deltaTime);
         transform.Translate(Vector3.up * jumpSpeed * characterJumpSpeed * Time.deltaTime);
+
+        //Karakter aşağıda ki hareketlerden birini yapıyor mu?
+        bool isWalkingVertical = verticalSpeed != 0;
+        bool isWalkingHorizontal = horizontalSpeed != 0;
+
+        //Karakter hareket ediyosa setBool ile parametre bilgilerini güncelle
+        animator.SetBool("isForward", isWalkingVertical);
+        animator.SetBool("isRight", isWalkingHorizontal);
+
+        // Hangi hızda hareket ettiğini hesapla (dikey ve yatay hızların büyüklüğü) 5 katı olan kod
+        //float movementSpeed = Mathf.Sqrt((verticalSpeed * verticalSpeed) + (horizontalSpeed * horizontalSpeed)) * characterSpeed;
+
+        // Hangi hızda hareket ettiğini hesapla (dikey ve yatay hızların büyüklüğü)
+        float movementSpeed = Mathf.Sqrt(verticalSpeed * verticalSpeed + horizontalSpeed * horizontalSpeed);
+
+        // Animasyon hızını karakterin hareket hızına göre ayarla
+        animator.speed = movementSpeed > 0 ? movementSpeed : 1f;  // Hareket varsa hız, yoksa normal hızda oynat
     }
 }
